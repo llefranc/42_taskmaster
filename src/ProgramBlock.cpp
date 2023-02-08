c1fd0c11fd1d85edeaa63ed92c1ed202b4473218
Helene Herin <hherin@student.42.fr>
2023-02-08 17:20:06 +0100

    init ProgramBlock

--- src/ProgramBlock.cpp
+++ src/ProgramBlock.cpp
@@ -12,3 +12,199 @@
 
 #include "ProgramBlock.hpp"
 
+ProgramBlock::ProgramBlock()
+{
+
+}
+
+ProgramBlock::ProgramBlock(const ProgramBlock& cp)
+{
+	this->state_ = cp.getState();
+	this->procs_ = cp.getProcs();
+	this->name_ = cp.getName();
+	this->cmd_ = cp.getCommand();
+	this->numprocs_ = cp.getNumprocs();
+	this->umask_ = cp.getUnamsk();
+	this->workDir_ = cp.getWorkdir();
+	this->autoStart_ = cp.getAutoStart();
+	this->autoRestart_ = cp.getAutoRestart();
+	this->startRetries_ = cp.getStartRetries();
+	this->startTime_ = cp.getStartTime();
+	this->exitCodes_ = cp.getExitCode();
+	this->stopSignal_ = cp.getStopSignal();
+	this->logOut_ = cp.getLogOut();
+	this->logErr_ = cp.getLogErr();
+	this->env_ = cp.getEnv();
+}
+
+ProgramBlock::~ProgramBlock()
+{
+
+}
+
+ProgramBlock& ProgramBlock::operator=(const ProgramBlock& pb)
+{
+	(void)pb;
+	return *this;
+}
+
+void ProgramBlock::setState(int state)
+{
+	state_ = state;
+}
+
+const int& ProgramBlock::getState() const
+{
+	return state_;
+}
+
+void ProgramBlock::setProcs(const std::vector<std::pair<int, long> >& proc)
+{
+	procs_ = proc;
+}
+
+const std::vector<std::pair<int, long> >& ProgramBlock::getProcs() const
+{
+	return procs_;
+}
+
+void ProgramBlock::setName(const std::string& name)
+{
+	name_ = name;
+}
+
+const std::string& ProgramBlock::getName() const
+{
+	return name_;
+}
+
+void ProgramBlock::setCommand(const std::string& cmd)
+{
+	cmd_=cmd;
+}
+
+const std::string& ProgramBlock::getCommand() const
+{
+	return cmd_;
+}
+
+void ProgramBlock::setNumprocs(const int numprocs)
+{
+	numprocs_ = numprocs;
+}
+
+const int& ProgramBlock::getNumprocs() const
+{
+	return numprocs_;
+}
+
+void ProgramBlock::setUnamsk(const std::string& umask)
+{
+	umask_ = umask;
+}
+
+const std::string& ProgramBlock::getUnamsk() const
+{
+	return umask_;
+}
+
+void ProgramBlock::setWorkdir(const std::string& workdir)
+{
+	workDir_ = workdir;
+}
+
+const std::string& ProgramBlock::getWorkdir() const
+{
+	return workDir_;
+}
+
+void ProgramBlock::setAutoStart(const bool autoStart)
+{
+	autoStart_ = autoStart;
+}
+
+const bool& ProgramBlock::getAutoStart() const
+{
+	return autoStart_;
+}
+
+void ProgramBlock::setAutoRestart(const int autoRestart)
+{
+	autoRestart_ = autoRestart;
+}
+
+const int& ProgramBlock::getAutoRestart() const
+{
+	return autoRestart_;
+}
+
+void ProgramBlock::setStartRetries(const int retries)
+{
+	startRetries_ = retries;
+}
+
+const int& ProgramBlock::getStartRetries() const
+{
+	return startRetries_;
+}
+
+
+void ProgramBlock::setStartTime(const int startTime)
+{
+	startTime_ = startTime;
+}
+
+const int& ProgramBlock::getStartTime() const
+{
+	return startTime_;
+}
+
+void ProgramBlock::setExitCode(const std::vector<int>& exitCode)
+{
+	exitCodes_ = exitCode;
+}
+
+const std::vector<int>& ProgramBlock::getExitCode() const
+{
+	return exitCodes_;
+}
+
+void ProgramBlock::setStopSignal(const int stopSignal)
+{
+	stopSignal_ = stopSignal;
+}
+
+const int& ProgramBlock::getStopSignal() const
+{
+	return stopSignal_;
+}
+
+void ProgramBlock::setLogOut(const std::string& logOut)
+{
+	logOut_ = logOut;
+}
+
+const std::string& ProgramBlock::getLogOut() const
+{
+	return logOut_;
+}
+
+void ProgramBlock::setLogErr(const std::string& logErr)
+{
+	logErr_ = logErr;
+}
+
+const std::string& ProgramBlock::getLogErr() const
+{
+	return logErr_;
+}
+
+void ProgramBlock::setEnv(const std::vector<std::string>& env)
+{
+	env_ = env;
+}
+
+const std::vector<std::string>& ProgramBlock::getEnv() const
+{
+	return env_;
+}
--- src/ProgramBlock.hpp
+++ src/ProgramBlock.hpp
@@ -27,19 +27,54 @@ class ProgramBlock
 			STATE_NEW,
 			STATE_UPDATED,
 			STATE_NEED_RM,
-		};
+		} E_prgmState;
 
 		enum {
 			AUTO_FALSE,
 			AUTO_TRUE,
 			AUTO_UNEXP,
-		};
+		} E_auto;
 
 		ProgramBlock();
 		ProgramBlock(const ProgramBlock& copyObj);
 		~ProgramBlock();
-		ProgramBlock& operator=(const ProgramBlock assignObj);
+		ProgramBlock& operator=(const ProgramBlock& assignObj);
 		// operator==
+
+		/* SETTERS & GETTERS */
+		void setState(int state);
+		const int& getState() const;
+		void setProcs(const std::vector<std::pair<int, long> >& proc);
+		const std::vector<std::pair<int, long> >& getProcs() const;
+		void setName(const std::string& name);
+		const std::string& getName() const;
+		void setCommand(const std::string& cmd);
+		const std::string& getCommand() const;
+		void setNumprocs(const int numprocs);
+		const int& getNumprocs() const;
+		void setUnamsk(const std::string& umask);
+		const std::string& getUnamsk() const;
+		void setWorkdir(const std::string& workdir);
+		const std::string& getWorkdir() const;
+		void setAutoStart(const bool autoStart);
+		const bool& getAutoStart() const;
+		void setAutoRestart(const int autoRestart);
+		const int& getAutoRestart() const;
+		void setStartRetries(const int retries);
+		const int& getStartRetries() const;
+		void setStartTime(const int startTime);
+		const int& getStartTime() const;
+		void setExitCode(const std::vector<int>& exitCode);
+		const std::vector<int>& getExitCode() const;
+		void setStopSignal(const int stopSignal);
+		const int& getStopSignal() const;
+		void setLogOut(const std::string& logOut);
+		const std::string& getLogOut() const;
+		void setLogErr(const std::string& logErr);
+		const std::string& getLogErr() const;
+		void setEnv(const std::vector<std::string>& env);
+		const std::vector<std::string>& getEnv() const;
+
 
 	private:
 
@@ -53,12 +88,12 @@ class ProgramBlock
 		bool autoStart_;
 		int autoRestart_;
 		int startRetries_;
-		int startTime_;
+		int startTime_;			// SHOULD BE in proc obj
 		std::vector<int> exitCodes_;
 		int stopSignal_;
 		std::string logOut_;
 		std::string logErr_;
-		char** env_;
+		std::vector<std::string> env_;
 
 };
 
