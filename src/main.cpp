cd380038064a1e46593a7dbd1c7f3308863580e6
Helene Herin <hlne.herin@gmail.com>
2023-02-19 22:56:26 +0100

    main test

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,46 @@
 /* ************************************************************************** */
 
 #include <iostream>
-
+#include <fcntl.h>
 #include <stdio.h>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <vector>
 
-int main(int ac, char** av)
+#include "ProgramBlock.hpp"
+#include "ProcInfo.hpp"
+#include "Spawner.hpp"
+#include "Logger.hpp"
+#include "TaskMaster.hpp"
+
+int nbProcessZombies = 0;
+int nbZombiesCleaned = 0;
+
+int main(int ac, char** av, char** env)
 {
 	(void)ac;
 	(void)av;
+	(void)env;
+	try 
+	{
+		
+
+		// spawner.startProcess(pInfo, prgB);
+		TaskMaster tm;
+		tm.initLogger("./log.txt");
+
+		tm.initConfigParser("");
+		tm.shellRoutine();
+
+	}
+	catch (std::runtime_error & e)
+	{
+		std::cerr << e.what()  << " : "  << strerror(errno) << std::endl;
+	}
+
 	return 0;
 }
+
+
